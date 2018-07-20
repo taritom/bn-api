89d6a58c666373df329513f8b42a1726b7df35c1
Cayle Sharrock <CjS77@users.noreply.github.com>
2018-07-20 12:18:41 +0200

    Add health check endpoint (#30)
    * Add health check endpoint
    
    * Linting

--- src/routing.rs
+++ src/routing.rs
@@ -1,4 +1,4 @@
-use actix_web::{http::Method, App};
+use actix_web::{http::Method, App, HttpResponse};
 use config::Config;
 use controllers::*;
 use middleware::auth::AuthMiddleware;
@@ -8,13 +8,15 @@ pub fn routes(config: &Config, app: App<AppState>) -> App<AppState> {
     let mw = AuthMiddleware::new(&config.token_secret);
     let mw2 = mw.clone();
 
-    app.resource("/artists/{id}", |r| {
-        r.middleware(mw);
-        r.method(Method::GET).with(artists::show);
-        r.method(Method::POST).with(artists::create);
-        r.method(Method::PUT).with(artists::update);
-        r.method(Method::DELETE).with(artists::destroy);
-    }).resource("/artists", |r| {
+    app.resource("/status", |r| r.get().f(|_| HttpResponse::Ok()))
+        .resource("/artists/{id}", |r| {
+            r.middleware(mw);
+            r.method(Method::GET).with(artists::show);
+            r.method(Method::POST).with(artists::create);
+            r.method(Method::PUT).with(artists::update);
+            r.method(Method::DELETE).with(artists::destroy);
+        })
+        .resource("/artists", |r| {
             r.middleware(mw2);
             r.method(Method::GET).with(artists::index);
             r.method(Method::POST).with(artists::create);
