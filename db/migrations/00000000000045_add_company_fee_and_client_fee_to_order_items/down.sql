0d429e5cc0f41ff614c27436a1e88da809030508
Yuko Roodt <neonknight@tari.com>
2018-12-05 10:54:50 +0200

    Revert "Added down migrations"
    This reverts commit 05c7c82f0a4b9962906798e20e7a12453a63e956.

--- db/migrations/00000000000045_add_company_fee_and_client_fee_to_order_items/down.sql
+++ db/migrations/00000000000045_add_company_fee_and_client_fee_to_order_items/down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE order_items
-  DROP COLUMN company_fee_in_cents,
-  DROP COLUMN  client_fee_in_cents;
+
--- db/migrations/00000000000046_add_company_fee_and_client_fee_to_fee_schedule_ranges/down.sql
+++ db/migrations/00000000000046_add_company_fee_and_client_fee_to_fee_schedule_ranges/down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE fee_schedule_ranges
-  DROP COLUMN company_fee_in_cents,
-  DROP COLUMN client_fee_in_cents;
+
