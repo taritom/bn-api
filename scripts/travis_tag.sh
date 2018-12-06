e87ae6d148c72c562e16b881d5e0626b02089a93
Stanimal <sdbondi@users.noreply.github.com>
2018-12-06 10:37:32 +0200

    Put init ssh script back into travis_tag

--- scripts/travis_tag.sh
+++ scripts/travis_tag.sh
@@ -7,5 +7,7 @@ fi
 
 echo "+git checkout master"
 git checkout master
+echo "+./scripts/init-github-ssh.sh"
+./scripts/init-github-ssh.sh "$encrypted_319ed1854cd7_key" "$encrypted_319ed1854cd7_iv"
 echo "+./scripts/bump-version.sh --with-git"
 ./scripts/bump-version.sh --with-git
