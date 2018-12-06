e8f3b028502be8e6a9ffc25573462d9451d0d3a0
Mike the Tike <stringhandler@gmail.com>
2018-12-06 14:46:19 +0200

    Run builds in release (#656)
    * Run in release mode
    
    * Run builds in release mode

--- scripts/run-api-tests.sh
+++ scripts/run-api-tests.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Ensure we are in the root of the git repo
 cd $(git rev-parse --show-toplevel)
-ulimit -S -s 65536
-ulimit -s 65536
-ulimit -a
-cd db
-cargo run create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
+
+cd ../db
+cargo run --release create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
 cd ../api && cargo test --release
