6418a6ba27c4236361e322ae369d3dc8ca5ca59f
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-12-06 14:45:08 +0200

    Use sh instead of bash (#680)

--- db/scripts/init.sh
+++ db/scripts/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Run this script to update the database to the latest migration version
 diesel -V 2> /dev/null || cargo install diesel_cli --no-default-features --features postgres
