4adc1f154458a21dbcaffac7d0d97ce13178353a
SW van Heerden <swvheerden@gmail.com>
2018-12-11 16:14:07 +0200

    Update up.sql (#710)

--- db/migrations/00000000000051_make_fees_not_null/up.sql
+++ db/migrations/00000000000051_make_fees_not_null/up.sql
@@ -1,8 +1,8 @@
 UPDATE organizations
-SET event_fee_in_cents = 0 where event_fee_in_cents = NULL;
+SET event_fee_in_cents = 0 where event_fee_in_cents IS NULL;
 
 UPDATE events
-SET fee_in_cents = 0 where fee_in_cents = NULL;
+SET fee_in_cents = 0 where fee_in_cents IS NULL;
 
 ALTER TABLE organizations
     ALTER COLUMN event_fee_in_cents SET NOT NULL;
