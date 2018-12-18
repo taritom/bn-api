e8d9dad3a5299b37daebeadc73094063f957009d
Philip Robinson <simian@tari.com>
2018-12-18 11:12:10 +0200

    Fix migration (#766)

--- db/migrations/00000000000055_add_client_and_company_event_fees/down.sql
+++ db/migrations/00000000000055_add_client_and_company_event_fees/down.sql
@@ -1,5 +1,5 @@
 ALTER TABLE organizations DROP COLUMN client_event_fee_in_cents;
 ALTER TABLE organizations DROP COLUMN company_event_fee_in_cents;
 
-ALTER TABLE events ADD client_fee_in_cents BIGINT NULL;
-ALTER TABLE events ADD company_fee_in_cents BIGINT NULL;
+ALTER TABLE events DROP COLUMN client_fee_in_cents;
+ALTER TABLE events DROP COLUMN company_fee_in_cents;
