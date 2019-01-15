b64013b645747894db2614ece194b1078861776c
Mike the Tike <stringhandler@gmail.com>
2019-01-15 08:42:55 +0200

    Add redemption code data to ticket types (#871)

--- api/src/controllers/redemption_codes.rs
+++ api/src/controllers/redemption_codes.rs
@@ -3,7 +3,7 @@ use actix_web::Path;
 use bigneon_db::prelude::*;
 use db::Connection;
 use errors::BigNeonError;
-use models::UserDisplayTicketType;
+use models::*;
 
 #[derive(Deserialize)]
 pub struct PathParameters {
@@ -24,20 +24,29 @@ pub fn show(
         discount_in_cents: Option<i64>,
         hold_type: HoldTypes,
     }
-    let r = R {
-        ticket_type: UserDisplayTicketType::from_ticket_type(
-            &TicketType::find(hold.ticket_type_id, conn)?,
-            &FeeSchedule::find(
-                Organization::find_for_event(hold.event_id, conn)?.fee_schedule_id,
-                conn,
-            )?,
-            false,
+
+    let redemption_code = hold.redemption_code.clone();
+    let max_per_order = hold.max_per_order;
+    let discount_in_cents = hold.discount_in_cents;
+    let hold_type = hold.hold_type;
+
+    let ticket_type = UserDisplayTicketType::from_ticket_type_and_hold(
+        &TicketType::find(hold.ticket_type_id, conn)?,
+        &FeeSchedule::find(
+            Organization::find_for_event(hold.event_id, conn)?.fee_schedule_id,
             conn,
         )?,
-        redemption_code: hold.redemption_code.clone(),
-        max_per_order: hold.max_per_order,
-        discount_in_cents: hold.discount_in_cents,
-        hold_type: hold.hold_type,
+        false,
+        Some(hold),
+        conn,
+    )?;
+
+    let r = R {
+        ticket_type,
+        redemption_code,
+        max_per_order,
+        discount_in_cents,
+        hold_type,
     };
     return Ok(HttpResponse::Ok().json(r));
 }
--- api/src/models/user_display_ticket_type.rs
+++ api/src/models/user_display_ticket_type.rs
@@ -2,6 +2,7 @@ use bigneon_db::prelude::*;
 use chrono::NaiveDateTime;
 use diesel::PgConnection;
 use models::DisplayTicketPricing;
+use std::cmp;
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
@@ -16,6 +17,7 @@ pub struct UserDisplayTicketType {
     pub increment: i32,
     pub limit_per_person: u32,
     pub ticket_pricing: Option<DisplayTicketPricing>,
+    pub redemption_code: Option<String>,
 }
 
 impl UserDisplayTicketType {
@@ -23,6 +25,22 @@ impl UserDisplayTicketType {
         ticket_type: &TicketType,
         fee_schedule: &FeeSchedule,
         box_office_pricing: bool,
+        conn: &PgConnection,
+    ) -> Result<UserDisplayTicketType, DatabaseError> {
+        UserDisplayTicketType::from_ticket_type_and_hold(
+            ticket_type,
+            fee_schedule,
+            box_office_pricing,
+            None,
+            conn,
+        )
+    }
+
+    pub fn from_ticket_type_and_hold(
+        ticket_type: &TicketType,
+        fee_schedule: &FeeSchedule,
+        box_office_pricing: bool,
+        hold: Option<Hold>,
         conn: &PgConnection,
     ) -> Result<UserDisplayTicketType, DatabaseError> {
         let mut status = ticket_type.status;
@@ -48,7 +66,7 @@ impl UserDisplayTicketType {
             }
         }
 
-        Ok(UserDisplayTicketType {
+        let mut result = UserDisplayTicketType {
             id: ticket_type.id,
             name: ticket_type.name.clone(),
             description: ticket_type.description.clone(),
@@ -57,8 +75,34 @@ impl UserDisplayTicketType {
             end_date: ticket_type.end_date,
             ticket_pricing,
             available,
+            redemption_code: hold.as_ref().map(|h| h.redemption_code.to_string()),
             increment: ticket_type.increment,
             limit_per_person: ticket_type.limit_per_person as u32,
-        })
+        };
+
+        if let Some(h) = hold {
+            result.description = Some(format!("Using promo code: {}", h.redemption_code));
+            result.limit_per_person = h.max_per_order.unwrap_or(0) as u32;
+            result.available = h.quantity(conn)?.1;
+            match h.hold_type {
+                HoldTypes::Comp => {
+                    result.ticket_pricing = result.ticket_pricing.map(|tp| DisplayTicketPricing {
+                        fee_in_cents: 0,
+                        price_in_cents: 0,
+                        ..tp
+                    })
+                }
+                HoldTypes::Discount => {
+                    result.ticket_pricing = result.ticket_pricing.map(|tp| DisplayTicketPricing {
+                        price_in_cents: cmp::max(
+                            0,
+                            tp.price_in_cents - h.discount_in_cents.unwrap_or(0),
+                        ),
+                        ..tp
+                    })
+                }
+            }
+        }
+        Ok(result)
     }
 }
