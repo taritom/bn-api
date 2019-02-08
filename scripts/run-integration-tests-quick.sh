1b1e78d41ae552e15d6da32b7a53343cb3958fe8
Stan Bondi <sdbondi@users.noreply.github.com>
2019-02-08 09:01:36 +0200

    Drone manifest and scripts (#999)
    * Drone manifest and scripts
    
    * only show api logs for integration tests in the event of a failure
    
    * copy pasta error - correct tag-and-commit script

---
+++ scripts/run-integration-tests-quick.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+./target/release/bndb_cli create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883 || {
+    echo "Migrations failed"
+    exit 1
+}
+
+./target/release/server -t false &> /tmp/api.log &
+export SERVER_PID=$!$1
+
+# Run newman tests
+newman run --timeout-request 60000 ./integration-tests/bigneon-tests.postman_collection.json -e ./integration-tests/travis.postman_environment.json
+NEWMAN_EXIT_CODE=$?
+kill -s SIGTERM $SERVER_PID
+
+if [[ $NEWMAN_EXIT_CODE -ne 0 ]]
+then
+    cat /tmp/api.log
+    exit $NEWMAN_EXIT_CODE
+fi
---
+++ scripts/run-other-tests-quick.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./target/release/bndb_cli create -c $DATABASE_URL -f -e superuser@test.com -p password -m 8883
+cargo test --release --exclude bigneon_api --all
