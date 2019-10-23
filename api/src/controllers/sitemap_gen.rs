47d4e4cfa6eb9e74abb9b166297a435b8b9c96bc
Mike the Tike <mikethetike@tari.com>
2019-10-23 12:08:26 +0200

    Add venues and orgs to sitemap (#1532)
    * Add venues and orgs to sitemap
    
    * Fix tests

--- api/src/controllers/sitemap_gen.rs
+++ api/src/controllers/sitemap_gen.rs
@@ -1,35 +1,73 @@
-use actix_web::HttpResponse;
-use bigneon_db::prelude::{Event, Slug};
+use actix_web::{HttpResponse, State};
+use bigneon_db::prelude::*;
+use chrono::Utc;
 use db::Connection;
 use errors::BigNeonError;
-use std::env;
+use itertools::Itertools;
+use server::AppState;
 use utils::gen_sitemap;
 
-pub fn index(connection: (Connection)) -> Result<HttpResponse, BigNeonError> {
+pub fn index(
+    (connection, state): (Connection, State<AppState>),
+) -> Result<HttpResponse, BigNeonError> {
+    let conn = connection.get();
     //find all active events
-    let events = Event::find_all_active_events(connection.get())?;
+    let events = Event::find_all_active_events(conn)?;
     let slug_ids = events
         .iter()
+        .filter(|e| e.publish_date.is_some() && e.publish_date.unwrap() < Utc::now().naive_utc())
         .map(|e| e.slug_id)
         .filter(|s| s.is_some())
         .map(|s| s.unwrap())
-        .collect::<Vec<_>>();
+        .collect_vec();
 
-    let slugs = Slug::find_all(slug_ids, connection.get())?;
+    let slugs = Slug::find_all(slug_ids, conn)?;
 
-    let slug_s = slugs
+    let mut urls = slugs
         .iter()
-        .map(|slug| create_url(&slug.slug))
-        .collect::<Vec<_>>();
+        .map(|slug| format!("{}/tickets/{}", state.config.front_end_url, &slug.slug))
+        .collect_vec();
 
-    let sitemap_xml = gen_sitemap::create_sitemap(&slug_s)?;
+    // Venues
+    let venue_slugs = Venue::all(None, conn)?
+        .iter()
+        .map(|venue| venue.slug_id)
+        .filter(|s| s.is_some())
+        .map(|s| s.unwrap())
+        .collect_vec();
+
+    let slugs = Slug::find_all(venue_slugs, conn)?;
+
+    for s in slugs
+        .iter()
+        .map(|slug| format!("{}/venues/{}", state.config.front_end_url, &slug.slug))
+    {
+        urls.push(s);
+    }
+
+    // Organizations
+    let organizations_slugs = Organization::all(conn)?
+        .iter()
+        .map(|org| org.slug_id)
+        .filter(|s| s.is_some())
+        .map(|s| s.unwrap())
+        .collect_vec();
+
+    let slugs = Slug::find_all(organizations_slugs, conn)?;
+
+    for s in slugs.iter().map(|slug| {
+        format!(
+            "{}/organizations/{}",
+            state.config.front_end_url, &slug.slug
+        )
+    }) {
+        urls.push(s);
+    }
+
+    let sitemap_xml = gen_sitemap::create_sitemap(&urls)?;
 
     Ok(HttpResponse::Ok()
         .content_type("text/xml")
         .body(sitemap_xml)
         .into())
-}
-
-fn create_url(slug: &str) -> String {
-    format!("{}/tickets/{}", env::var("FRONT_END_URL").unwrap(), slug)
 }
