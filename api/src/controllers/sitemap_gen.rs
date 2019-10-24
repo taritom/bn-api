ceaf3ef47293ed0e1bd44875f53f1d5668f0482b
toiletspider <dan4rie@gmail.com>
2019-10-24 08:14:13 +0200

    refactrored sitemap url generations

--- api/src/controllers/sitemap_gen.rs
+++ api/src/controllers/sitemap_gen.rs
@@ -6,14 +6,15 @@ use errors::BigNeonError;
 use itertools::Itertools;
 use server::AppState;
 use utils::gen_sitemap;
+use diesel::PgConnection;
 
 pub fn index(
     (connection, state): (Connection, State<AppState>),
 ) -> Result<HttpResponse, BigNeonError> {
     let conn = connection.get();
+
     //find all active events
-    let events = Event::find_all_active_events(conn)?;
-    let slug_ids = events
+    let events_slug_ids = Event::find_all_active_events(conn)?
         .iter()
         .filter(|e| e.publish_date.is_some() && e.publish_date.unwrap() < Utc::now().naive_utc())
         .map(|e| e.slug_id)
@@ -21,48 +22,31 @@ pub fn index(
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let slugs = Slug::find_all(slug_ids, conn)?;
-
-    let mut urls = slugs
-        .iter()
-        .map(|slug| format!("{}/tickets/{}", state.config.front_end_url, &slug.slug))
-        .collect_vec();
+    let event_urls = create_urls( &state.config.front_end_url, events_slug_ids, "tickets".to_string(), conn);
 
     // Venues
-    let venue_slugs = Venue::all(None, conn)?
+    let venue_slugs_ids = Venue::all(None, conn)?
         .iter()
         .map(|venue| venue.slug_id)
         .filter(|s| s.is_some())
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let slugs = Slug::find_all(venue_slugs, conn)?;
-
-    for s in slugs
-        .iter()
-        .map(|slug| format!("{}/venues/{}", state.config.front_end_url, &slug.slug))
-    {
-        urls.push(s);
-    }
+    let mut venue_urls = create_urls( &state.config.front_end_url, venue_slugs_ids, "venues".to_string(), conn);
 
     // Organizations
-    let organizations_slugs = Organization::all(conn)?
+    let organizations_slugs_ids = Organization::all(conn)?
         .iter()
         .map(|org| org.slug_id)
         .filter(|s| s.is_some())
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let slugs = Slug::find_all(organizations_slugs, conn)?;
+    let organizations_urls = create_urls( &state.config.front_end_url, organizations_slugs_ids, "organizations".to_string(), conn);
 
-    for s in slugs.iter().map(|slug| {
-        format!(
-            "{}/organizations/{}",
-            state.config.front_end_url, &slug.slug
-        )
-    }) {
-        urls.push(s);
-    }
+    let mut urls = event_urls;
+    urls.extend(venue_urls);
+    urls.extend(organizations_urls);
 
     let sitemap_xml = gen_sitemap::create_sitemap(&urls)?;
 
@@ -70,4 +54,10 @@ pub fn index(
         .content_type("text/xml")
         .body(sitemap_xml)
         .into())
+}
+
+fn create_urls(front_url: &String, slug_ids: Vec<String>, url_parm: String, conn: &PgConnection) -> Vec<String> {
+    let slugs = Slug::find_all(slug_ids, conn)?;
+    let gen_urls = slugs.iter().map(|slug| format!("{}/{}/{}", front_url, url_parm, slug));
+    gen_urls
 }
