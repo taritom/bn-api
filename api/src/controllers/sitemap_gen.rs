460c57d8816099c81ed5b195b8b2107f07b32e41
toiletspider <dan4rie@gmail.com>
2019-10-24 08:36:02 +0200

    fmt

--- api/src/controllers/sitemap_gen.rs
+++ api/src/controllers/sitemap_gen.rs
@@ -2,17 +2,16 @@ use actix_web::{HttpResponse, State};
 use bigneon_db::prelude::*;
 use chrono::Utc;
 use db::Connection;
+use diesel::PgConnection;
 use errors::BigNeonError;
 use itertools::Itertools;
 use server::AppState;
 use utils::gen_sitemap;
-use diesel::PgConnection;
 use uuid::Uuid;
 
 pub fn index(
     (connection, state): (Connection, State<AppState>),
 ) -> Result<HttpResponse, BigNeonError> {
-
     let conn = connection.get();
 
     //find all active events
@@ -24,7 +23,12 @@ pub fn index(
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let event_urls = create_urls( &state.config.front_end_url, events_slug_ids, "tickets".to_string(), conn);
+    let event_urls = create_urls(
+        &state.config.front_end_url,
+        events_slug_ids,
+        "tickets".to_string(),
+        conn,
+    );
 
     // Venues
     let venue_slugs_ids = Venue::all(None, conn)?
@@ -34,7 +38,12 @@ pub fn index(
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let venue_urls = create_urls( &state.config.front_end_url, venue_slugs_ids, "venues".to_string(), conn);
+    let venue_urls = create_urls(
+        &state.config.front_end_url,
+        venue_slugs_ids,
+        "venues".to_string(),
+        conn,
+    );
 
     // Organizations
     let organizations_slugs_ids = Organization::all(conn)?
@@ -44,7 +53,12 @@ pub fn index(
         .map(|s| s.unwrap())
         .collect_vec();
 
-    let organizations_urls = create_urls( &state.config.front_end_url, organizations_slugs_ids, "organizations".to_string(), conn);
+    let organizations_urls = create_urls(
+        &state.config.front_end_url,
+        organizations_slugs_ids,
+        "organizations".to_string(),
+        conn,
+    );
 
     let mut urls = event_urls;
     urls.extend(venue_urls);
@@ -58,8 +72,16 @@ pub fn index(
         .into())
 }
 
-fn create_urls(front_url: &String, slug_ids: Vec<Uuid>, url_parm: String, conn: &PgConnection) -> Vec<String> {
+fn create_urls(
+    front_url: &String,
+    slug_ids: Vec<Uuid>,
+    url_parm: String,
+    conn: &PgConnection,
+) -> Vec<String> {
     let slugs = Slug::find_all(slug_ids, conn).unwrap();
-    let gen_urls = slugs.iter().map(|slug| format!("{}/{}/{}", front_url, url_parm, slug.slug)).collect_vec();
+    let gen_urls = slugs
+        .iter()
+        .map(|slug| format!("{}/{}/{}", front_url, url_parm, slug.slug))
+        .collect_vec();
     gen_urls
 }
