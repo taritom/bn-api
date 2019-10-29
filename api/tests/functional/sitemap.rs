b33fda096b405aa81ac9d9462a40b4bfe5d6fb65
toiletspider <dan4rie@gmail.com>
2019-10-29 08:06:01 +0200

    wip

--- api/tests/functional/sitemap.rs
+++ api/tests/functional/sitemap.rs
@@ -1,14 +1,10 @@
 use support::database::TestDatabase;
-use support;
-use bigneon_db::models::{Roles};
-use bigneon_db::prelude::*;
+//use bigneon_db::prelude::*;
 use bigneon_api::utils::gen_sitemap;
 
 #[test]
 fn index() {
     let database = TestDatabase::new();
-    let user = database.create_user().finish();
-    let auth_user = support::create_auth_user_from_user(&user, Roles::User, None, &database);
     let connection = database.connection.get();
     let organization = database.create_organization().finish();
     let venue = database
@@ -17,23 +13,12 @@ fn index() {
         .with_state("California".to_string())
         .with_country("US".to_string())
         .finish();
-    let venue2 = database
-        .create_venue()
-        .with_city("Oakland".to_string())
-        .with_state("California".to_string())
-        .with_country("US".to_string())
-        .finish();
+
     let event = database
         .create_event()
         .with_name("NewEvent1".to_string())
         .with_organization(&organization)
         .with_venue(&venue)
-        .finish();
-    let event2 = database
-        .create_event()
-        .with_name("NewEvent2".to_string())
-        .with_organization(&organization)
-        .with_venue(&venue2)
         .finish();
 
     let slug = database
@@ -41,17 +26,17 @@ fn index() {
         .for_event(&event)
         .with_slug("redirect-me")
         .finish();
-    let slug2 = database
-        .create_slug()
-        .for_venue(&venue, SlugTypes::Venue)
-        .with_slug("redirect-me2")
-        .finish();
-    let slug = database
-        .create_slug()
-        .for_organization(&organization)
-        .with_slug("redirect-me3")
-        .finish();
 
-    let sitemap_xml = gen_sitemap::create_sitemap_conn(conn, &state.config.front_end_url)?;
+    let urls: Vec<String> = Vec::new();
+    println!("{:?}, {:?}, {:?}, ", venue, event, slug);
+    let sitemap_xml_test = gen_sitemap::create_sitemap(&urls).unwrap();
 
+    let sitemap_xml = gen_sitemap::create_sitemap_conn(connection, &"front_end_url".to_string()).unwrap();
+    println!("{}", sitemap_xml);
+    assert_eq!(sitemap_xml_test, sitemap_xml);
 }
+
+//fn check_if_url_included(url: &String, sitemap_xml: &String) -> bool {
+// true
+//}
+
