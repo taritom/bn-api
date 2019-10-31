353d1481dd3f62ec8c075cb2bcf20cec4803109b
Mike the Tike <mikethetike@tari.com>
2019-10-31 11:15:32 +0200

    Update db/src/models/broadcasts.rs

--- db/src/models/broadcasts.rs
+++ db/src/models/broadcasts.rs
@@ -93,7 +93,7 @@ impl Broadcast {
         diesel::update(&broadcast)
             .set(broadcasts::dsl::opened_quantity.eq(broadcast.opened_quantity + 1))
             .get_result(connection)
-            .to_db_error(ErrorCode::UpdateError, "Unable to update broadcast")
+            .to_db_error(ErrorCode::UpdateError, "Unable to update open count on broadcast")
     }
 
     pub fn increment_sent_count(
