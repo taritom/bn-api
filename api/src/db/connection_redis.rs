07cd2ca632ae94b3deb9d072eadeb617f1542ac2
toiletspider <dan4rie@gmail.com>
2019-11-22 13:49:26 +0200

    wip

--- api/src/db/connection_redis.rs
+++ api/src/db/connection_redis.rs
@@ -26,7 +26,7 @@ pub trait RedisCommands {
     fn set_value_int(&mut self, key: &str, value: u32) -> RedisResult<u32>;
     fn get_cache_value<T: ?Sized>(&mut self, query: &T, time_lapse: u32);
     fn set_cache_value<T: ?Sized>(&mut self, query: &T, cached_value: &str, time_lapse: u32);
-    fn in_current_period(unix_time: u32, period: u32) -> bool;
+    fn is_key_outdated(unix_time: u32, period: u32) -> bool;
 }
 
 impl RedisCommands for r2d2_redis::r2d2::PooledConnection<RedisConnectionManager> {
@@ -46,15 +46,15 @@ impl RedisCommands for r2d2_redis::r2d2::PooledConnection<RedisConnectionManager
     // query: this is the structure that will be serialized to json
     // time_lapse: this is measured in seconds. Only return the redis value if it happened in this period
     fn get_cache_value<T: ?Sized>(&mut self, query: &T, time_lapse: u32) -> Option<String> {
-        let query_serialized = serde_json::to_string(query)?;
-        if let Some(set_time) = self.get_value_int(query_serialized.borrow()) {
+        let key = serde_json::to_string(query)?; // the query is the key
+        if let Some(set_time) = self.get_value_int(key.borrow()) {
             // get the time when query was set
-            let b = self.in_current_period(set_time, time_lapse);
+            let is_out_dated = self.is_key_outdated(set_time, time_lapse);
         }
 
         if let Some(seconds) = time_period {}
         format!("{}{}", foo, bar)
-        match self.get_value(query_serialized.borrow()) {
+        match self.get_value(key.borrow()) {
             Ok(cached_value) => {
                 Some(cached_value)
             },
@@ -66,14 +66,13 @@ impl RedisCommands for r2d2_redis::r2d2::PooledConnection<RedisConnectionManager
         unimplemented!()
     }
 
-    // returns true of the giving number of seconds falls in the current period
     // start_time: this is measured in Unix time, the time in milliseconds from 1970-01-01
-    fn in_current_period(start_time: u32, period: u32) -> bool {
+    fn is_key_outdated(start_time: u32, seconds: u32) -> bool {
         let utc: DateTime<Utc> = Utc::now();
-        if (utc.timestamp_millis() - start_time)*1000 > seconds {
-            false
+        if (utc.timestamp_millis() - start_time)*1000 > seconds as i64 {
+            true
         }
-        true
+        false
     }
 }
 
