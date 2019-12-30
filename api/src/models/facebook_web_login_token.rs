2f956ddbe93440f8a6955f7d5fb568afe356b471
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2019-12-30 13:04:23 +0200

    Facebook can return a negative number for immediate expiry (#1646)

--- api/src/models/facebook_web_login_token.rs
+++ api/src/models/facebook_web_login_token.rs
@@ -5,7 +5,7 @@ pub struct FacebookWebLoginToken {
     #[serde(rename = "accessToken")]
     pub access_token: String,
     #[serde(rename = "expiresIn")]
-    pub expires_in: u64,
+    pub expires_in: i64,
     #[serde(rename = "signedRequest")]
     pub signed_request: String,
     // This is not a Facebook field, but rather tells the API whether
