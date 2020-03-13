48b421223c4b962966005361c44e1cf051fe919b
Tim Lovett <timo614@gmail.com>
2020-03-13 15:25:58 -0400

    Remove cancelled ticket types from scan counts report (#1716)
    Co-authored-by: Keith Simon <41575888+Krakaw@users.noreply.github.com>

--- db/src/queries/reports/reports_scan_counts.sql
+++ db/src/queries/reports/reports_scan_counts.sql
@@ -1,6 +1,6 @@
 SELECT
   COUNT(*) OVER ()                                                                          AS total,
-  CASE WHEN tt.status = 'Cancelled' THEN concat(tt.name, ' (Cancelled)') ELSE tt.name END   AS ticket_type_name,
+  tt.name                                                                                   AS ticket_type_name,
   CAST(COALESCE(COUNT(DISTINCT ti.id) FILTER(WHERE ti.status = 'Redeemed'), 0) AS BIGINT)   AS scanned_count,
   CAST(COALESCE(COUNT(DISTINCT ti.id) FILTER(WHERE ti.status = 'Purchased'), 0) AS BIGINT)  AS not_scanned_count
 FROM ticket_types tt
@@ -9,6 +9,7 @@ LEFT JOIN ticket_instances ti ON a.id = ti.asset_id
 -- Confirm this isn't a refunded redeemed (they keep their redeemed status and order association unlike normal refunds)
 LEFT JOIN refunded_tickets rt ON rt.ticket_instance_id = ti.id AND ti.order_item_id = rt.order_item_id
 WHERE tt.event_id = $1
+AND tt.status <> 'Cancelled'
 AND rt.id IS NULL
 GROUP BY tt.name, tt.rank, tt.status
 ORDER BY tt.rank;
