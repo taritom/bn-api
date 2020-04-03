3940ac5f5dc5e5ecb1a3f8d690b8d316d1b6cf6b
taritom <taritom>
2020-04-02 19:13:24 -0500

    Making unused fields optional.

--- api/src/models/facebook_web_login_token.rs
+++ api/src/models/facebook_web_login_token.rs
@@ -5,9 +5,9 @@ pub struct FacebookWebLoginToken {
     #[serde(rename = "accessToken")]
     pub access_token: String,
     #[serde(rename = "expiresIn")]
-    pub expires_in: i64,
+    pub expires_in: Option<i64>,
     #[serde(rename = "signedRequest")]
-    pub signed_request: String,
+    pub signed_request: Option<String>,
     // This is not a Facebook field, but rather tells the API whether
     // to link this token to the current user. The casing is done to match
     // the other fields
